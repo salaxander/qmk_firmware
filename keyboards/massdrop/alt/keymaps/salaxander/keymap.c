331218da9a6a69e029c643ff8943834dbf259457
Xander Grzywinski <xandergrzy@outlook.com>
2018-12-12 21:53:54 -0800

    fix backslash escape

--- keyboards/massdrop/alt/keymaps/salaxander/keymap.c
+++ keyboards/massdrop/alt/keymaps/salaxander/keymap.c
@@ -190,7 +190,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case SHRUG:
             if (record->event.pressed) {
-                SEND_STRING("¯\_(ツ)_/¯"); // this is our macro!
+                SEND_STRING("¯\\_(ツ)_/¯"); // this is our macro!
             }
             return false;
         default:
